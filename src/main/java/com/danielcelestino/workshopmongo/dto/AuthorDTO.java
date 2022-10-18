06e9c8824b6c405bb0116288b40d7289c1c3f3af
Daniel Celestino <danielcelestino@gmail.com>
2022-10-17 22:44:46 -0300

    Creating AuthorDTO Entity

--- src/main/java/com/danielcelestino/workshopmongo/dto/AuthorDTO.java
+++ src/main/java/com/danielcelestino/workshopmongo/dto/AuthorDTO.java
@@ -4,20 +4,19 @@ import java.io.Serializable;
 
 import com.danielcelestino.workshopmongo.domain.User;
 
-public class UserDTO implements Serializable {
+public class AuthorDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private String id;
 	private String name;
-	private String email;
 	
-	public UserDTO() {
+	public AuthorDTO() {
+		super();
 	}
 	
-	public UserDTO(User obj) {
+	public AuthorDTO(User obj) {
 		id = obj.getId();
 		name = obj.getName();
-		email = obj.getEmail();
 	}
 
 	public String getId() {
@@ -36,13 +35,5 @@ public class UserDTO implements Serializable {
 		this.name = name;
 	}
 
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
 	
 }
